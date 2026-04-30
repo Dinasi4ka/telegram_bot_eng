811c38b58fd4803dc555dd1d5368235b95b97514
Diana <114948133+Dinasi4ka@users.noreply.github.com>
2026-04-30 16:12:25 +0300

    Added срфтпуі

--- bot.py
+++ bot.py
@@ -49,6 +49,7 @@ async def main():
     await run_web()
 
     logging.info("EduBot запущено ✅")
+    await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
 
 
